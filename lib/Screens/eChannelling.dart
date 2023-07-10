d27c23a1c1afb47845165159bfe4f74d9b474a34
aathavan01 <aathavan4580@gmail.com>
2023-07-10 15:57:58 +0530

    order place issue fixed

--- lib/Screens/eChannelling.dart
+++ lib/Screens/eChannelling.dart
@@ -24,6 +24,7 @@ class _EChannellingState extends State<EChannelling> {
   List<String> filteredDoctorsList = [];
 
   TextEditingController _searchController = TextEditingController();
+  FocusNode _searchFocusNode = FocusNode();
 
   @override
   void initState() {
@@ -105,178 +106,164 @@ class _EChannellingState extends State<EChannelling> {
     return Scaffold(
       appBar: MyAppBar(),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 500, // Adjust the desired height of the image
-                  child: Image.asset('assets/images/eChannelling.png', fit: BoxFit.cover),
-                ),
-                Positioned(
-                  bottom: 10,
-                  right: 80,
-                  child: RichText(
-                    text: TextSpan(
-                      style: TextStyle(fontSize: 25.0, color: Colors.black),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Welcome',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: ' to Capsule\n eChannelling Network!',
-                          style: TextStyle(fontStyle: FontStyle.italic),
-                        ),
-                      ],
-                    ),
+        child: GestureDetector(
+          onTap: () {
+            _searchFocusNode.requestFocus();
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 500, // Adjust the desired height of the image
+                    child: Image.asset('assets/images/eChannelling.png', fit: BoxFit.cover),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 25),
-              child: Text(
-                'Select Type',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Row(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: RadioListTile<int>(
-                    title: Text(
-                      'Doctors',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 13),
-                    ),
-                    value: 0,
-                    groupValue: _selectedType,
-                    onChanged: (int? value) {
-                      setState(() {
-                        _selectedType = value!;
-                      });
-                    },
-                    contentPadding: EdgeInsets.zero, // Remove padding around the text
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: RadioListTile<int>(
-                    title: Text(
-                      'Specialization',
-                      style: TextStyle(fontSize: 13),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    value: 1,
-                    groupValue: _selectedType,
-                    onChanged: (int? value) {
-                      setState(() {
-                        _selectedType = value!;
-                      });
-                    },
-                    contentPadding: EdgeInsets.zero, // Remove padding around the text
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: RadioListTile<int>(
-                    title: Text(
-                      'Hospital',
-                      style: TextStyle(fontSize: 13),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    value: 2,
-                    groupValue: _selectedType,
-                    onChanged: (int? value) {
-                      setState(() {
-                        _selectedType = value!;
-                      });
-                    },
-                    contentPadding: EdgeInsets.zero, // Remove padding around the text
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: AutoCompleteTextField<String>(
-                key: GlobalKey<AutoCompleteTextFieldState<String>>(),
-                controller: _searchController,
-                suggestions: filteredDoctorsList,
-                clearOnSubmit: false,
-                decoration: InputDecoration(
-                  labelText: 'Search ${_getSelectedTypeName(_selectedType)} Name (Max 30 Characters)',
-                  labelStyle: TextStyle(fontWeight: FontWeight.w300),
-                  border: OutlineInputBorder(),
-                ),
-                itemFilter: (item, query) =>
-                    item.toLowerCase().contains(query.toLowerCase()),
-                itemSorter: (a, b) => a.compareTo(b),
-                itemSubmitted: (item) {
-                  setState(() {
-                    _searchController.text = item;
-                  });
-                },
-                itemBuilder: (context, item) {
-                  return ListTile(
-                    title: Text(item),
-                    onTap: () {
-                      setState(() {
-                        _searchController.text = item;
-                      });
-                    },
-                  );
-                },
-                textChanged: (value) {
-                  filterDoctorsList(value);
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0xff2AB29D),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SearchResult(
-                          selectedType: _getSelectedTypeName(_selectedType),
-                          searchValue: _searchController.text,
-                        ),
+                  Positioned(
+                    bottom: 10,
+                    right: 80,
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(fontSize: 25.0, color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Welcome',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ' to Capsule\n eChannelling Network!',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xff2AB29D),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
                   ),
-                  child: Text(
-                    "SEARCH",
-                    style: TextStyle(fontSize: 18),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Text(
+                  'Select Type',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: RadioListTile<int>(
+                      title: Text(
+                        'Doctors',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 13),
+                      ),
+                      value: 0,
+                      groupValue: _selectedType,
+                      onChanged: (int? value) {
+                        setState(() {
+                          _selectedType = value!;
+                        });
+                      },
+                      contentPadding: EdgeInsets.zero, // Remove padding around the text
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: RadioListTile<int>(
+                      title: Text(
+                        'Specialization',
+                        style: TextStyle(fontSize: 13),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      value: 1,
+                      groupValue: _selectedType,
+                      onChanged: (int? value) {
+                        setState(() {
+                          _selectedType = value!;
+                        });
+                      },
+                      contentPadding: EdgeInsets.zero, // Remove padding around the text
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: RadioListTile<int>(
+                      title: Text(
+                        'Hospital',
+                        style: TextStyle(fontSize: 13),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      value: 2,
+                      groupValue: _selectedType,
+                      onChanged: (int? value) {
+                        setState(() {
+                          _selectedType = value!;
+                        });
+                      },
+                      contentPadding: EdgeInsets.zero, // Remove padding around the text
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  focusNode: _searchFocusNode,
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Search ${_getSelectedTypeName(_selectedType)} Name (Max 30 Characters)',
+                    labelStyle: TextStyle(fontWeight: FontWeight.w300),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    filterDoctorsList(value);
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xff2AB29D),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SearchResult(
+                            selectedType: _getSelectedTypeName(_selectedType),
+                            searchValue: _searchController.text,
+                          ),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff2AB29D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                    ),
+                    child: Text(
+                      "SEARCH",
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
-  }}
+  }
+}
